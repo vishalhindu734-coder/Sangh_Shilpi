8936511537593bb1783ffbd89a8268c89cafc75c
vishalhindu734-coder <vishalhindu734@gmail.com>
2026-04-26 14:49:29 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig(({base:/Sangh_Shilpi/ mode }) => {
+export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       server: {
